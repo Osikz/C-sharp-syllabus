9cd7f5a6e12c88a725e87400f74d636f53a1bb58
Osikz <o.jaunslavietis@gmail.com>
2021-11-30 00:05:05 +0200

    Exercise_7 fixed.

--- csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
@@ -12,10 +12,12 @@ namespace Exercise_7
             int upperLetters = 0;
 
             for (int i = 0; i < userInput.Length; i++)
+            {
                 if (char.IsUpper(userInput[i]))
                 {
                     upperLetters++;
                 }
+            }
 
             Console.WriteLine($"There are {upperLetters} uppercase letters in provided text.");
             Console.ReadKey();
