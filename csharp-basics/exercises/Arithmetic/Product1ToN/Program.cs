35ac8f86aefa77151f1ac2929c836b4fa4c6c20b
Osikz <o.jaunslavietis@gmail.com>
2021-12-03 16:41:29 +0200

    Exercise_4 completed.

---
+++ csharp-basics/exercises/Arithmetic/Product1ToN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Product1ToN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int product = 1;
+
+            Console.WriteLine("Please enter the end point of product: ");
+            int.TryParse(Console.ReadLine(), out int end);
+
+            for (int i = start; i <= end; i++)
+            {
+                product *= i;
+            }
+
+            Console.WriteLine($"The product from {start} to {end} is {product}.");
+            Console.ReadKey();
+        }
+    }
+}
