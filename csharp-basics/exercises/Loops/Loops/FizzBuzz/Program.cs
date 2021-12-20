26f25b4b8fa2d3ecda630a9e3bd01ce16beed854
Osikz <o.jaunslavietis@gmail.com>
2021-12-20 20:26:36 +0200

    Exercise_6 completed

---
+++ csharp-basics/exercises/Loops/Loops/FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter MAX value:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else
+                {
+                    Console.Write($"{i} ");
+                }
+
+                if (i % 20 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
