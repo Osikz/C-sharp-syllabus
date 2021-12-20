6e9fc213e4c82a02f14c9b7b4b5cfcc51089050c
Osikz <o.jaunslavietis@gmail.com>
2021-12-20 20:53:09 +0200

    Exercise_8 completed

---
+++ csharp-basics/exercises/Loops/Loops/AsciiFigure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AsciiFigure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter desired size:");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = input; i > 0; i--)
+            {
+                for (int j = 0; j < i - 1; j++)
+                {
+                    Console.Write("////");
+                }
+                for (int e = input; e > i; e--)
+                {
+                    Console.Write("********");
+                }
+                for (int k = 0; k < i - 1; k++)
+                {
+                    Console.Write("\\\\\\\\");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
