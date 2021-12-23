a57849036dc29cf785104c2297a7f659a9f7d233
Osikz <o.jaunslavietis@gmail.com>
2021-12-23 23:14:21 +0200

    Exercise_1 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Product
+    {
+        private string _name;
+        private double _price;
+        private int _amount;
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            _name = name;
+            _price = priceAtStart;
+            _amount = amountAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name} - Price: {_price}EUR - Amount: {_amount}");
+        }
+
+        public void ChangeAmount(int newAmount)
+        {
+            _amount = newAmount;
+        }
+
+        public void ChangePrice(double newPrice)
+        {
+            _price = newPrice;
+        }
+    }
+}
