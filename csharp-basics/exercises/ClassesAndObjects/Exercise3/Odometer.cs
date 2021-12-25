cd33d1def6fd805cc1b771d13f716b6a682de584
Osikz <o.jaunslavietis@gmail.com>
2021-12-25 18:45:05 +0200

    Exercise_3 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Odometer.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercise3
+{
+    internal class Odometer
+    {
+        private int _mileage;
+        private FuelGauge _liters;
+
+        public Odometer(int mileage, FuelGauge liters)
+        {
+            _mileage = mileage;
+            _liters = liters;
+        }
+
+        public int OdoReading()
+        {
+            return _mileage;
+        }
+
+        public void Drive()
+        {
+            if (OdoReading() < 1000000)
+            {
+                _mileage++;
+            }
+            else
+            {
+                _mileage = 0;
+            }
+
+            if (OdoReading() % 10 == 0)
+            {
+                _liters.FuelConsume();
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fuel = new FuelGauge(0);
+            var odo = new Odometer(0, fuel);
+
+            fuel.FillUp();
+
+            while (fuel.FuelAmount() > 0)
+            {
+                odo.Drive();
+                Console.WriteLine($"Current Odometer Reading: {odo.OdoReading()}km " +
+                                  $"\r\nFuel remaining: {fuel.FuelAmount()}l\r\n");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
