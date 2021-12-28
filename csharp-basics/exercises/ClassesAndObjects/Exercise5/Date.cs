bdea193f6da28f0a45a33ba91090285fcd63f488
Osikz <o.jaunslavietis@gmail.com>
2021-12-28 18:30:30 +0200

    Exercise_5 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Date.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class Date
+    {
+        private int _year { get; set; }
+        private int _month { get; set; }
+        private int _day { get; set; }
+
+        public Date(int day, int month, int year)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{_day}/{_month}/{_year}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/DateTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal class DateTest
+    {
+        static void Main(string[] args)
+        {
+            var date = new Date(31, 12, 2021);
+            var date1 = new Date(24, 12, 2021);
+            var date2 = new Date(01, 01, 2022);
+
+            date.DisplayDate();
+            date1.DisplayDate();
+            date2.DisplayDate();
+
+            Console.ReadKey();
+        }
+    }
+}
