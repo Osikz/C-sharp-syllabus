3650bffe501d833736b00fb1c59bfbc6034ae909
Osikz <o.jaunslavietis@gmail.com>
2021-12-29 18:08:08 +0200

    Exercise_10 completed

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace BankAccount
 {
@@ -10,6 +7,37 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            var benben = new BankAccount("Benson", 17.50M);
+            var benneb = new BankAccount("Benson", -17.5431M);
+            var nebben = new BankAccount("Benson", 55512.773M);
+
+            Console.WriteLine(benben.ShowUserNameAndBalance());
+            Console.WriteLine(benneb.ShowUserNameAndBalance());
+            Console.WriteLine(nebben.ShowUserNameAndBalance());
+
+            Console.ReadKey();
+        }
+    }
+
+    class BankAccount
+    {
+        private string _name;
+        private decimal _balance;
+
+        public BankAccount(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            if (_balance < 0)
+            {
+                _balance *= -1;
+                return $"{_name}, -{_balance.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))}";
+            }
+            return $"{_name},  {_balance.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))}";
         }
     }
 }
