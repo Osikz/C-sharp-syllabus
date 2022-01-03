dfa736fb6c1729ff1f0b0f8bff5f6273890ee428
Osikz <o.jaunslavietis@gmail.com>
2022-01-03 17:20:39 +0200

    Exercise_3 completed

---
+++ csharp-basics/exercises/Collections/Exercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uniqueItems = new HashSet<string>();
+
+            Console.WriteLine("Enter name:");
+            var name = Console.ReadLine();
+
+            while (name != string.Empty)
+            {
+                Console.WriteLine("Enter name:");
+                name = Console.ReadLine();
+                uniqueItems.Add(name);
+            }
+
+            Console.WriteLine("Unique names:");
+
+            foreach (var i in uniqueItems)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
