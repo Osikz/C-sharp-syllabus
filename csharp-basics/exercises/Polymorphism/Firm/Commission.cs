150fd21ca87dd0f6f20bec06904d3cf06837babf
Osikz <o.jaunslavietis@gmail.com>
2022-01-06 22:17:56 +0200

    Exercise_4 completed

---
+++ csharp-basics/exercises/Polymorphism/Firm/Commission.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Firm
+{
+    public class Commission : Hourly
+    {
+        private double _totalSales;
+        private double _commissionRate;
+        public Commission(string eName, string eAddress, string ePhone, string socSecNumber, double rate, double commissionRate) : base(eName, eAddress, ePhone, socSecNumber, rate)
+        {
+            _commissionRate = commissionRate;
+        }
+
+        public void AddSales(double totalSales)
+        {
+            _totalSales = totalSales;
+        }
+
+        public override double Pay()
+        {
+            var payment = base.Pay() + _totalSales * _commissionRate;
+            _totalSales = 0;
+            return Math.Round(payment, 2);
+        }
+
+        public override string ToString() 
+        {
+            var result = base.ToString();
+            result += "\nTotal sales: $" + _totalSales;
+            return result;
+        }
+    }
+}
