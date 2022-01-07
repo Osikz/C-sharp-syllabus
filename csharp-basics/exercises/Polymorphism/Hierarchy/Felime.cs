40e65ea3b32b8edade1ab6ed1c88b8db3a5e4bd3
Osikz <o.jaunslavietis@gmail.com>
2022-01-07 13:06:43 +0200

    Exercise_6 completed

---
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Felime.cs
@@ -0,0 +1,10 @@
+﻿namespace Hierarchy
+{
+    public abstract class Felime : Mammal
+    {
+        public Felime(string animalName, string animalType, double animalWeight, int foodEaten, string livingRegion)
+            : base(animalName, animalType, animalWeight, foodEaten, livingRegion)
+        {
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Mammal.cs
@@ -0,0 +1,19 @@
+﻿namespace Hierarchy
+{
+    public abstract class Mammal : Animal
+    {
+        private string _livingRegion;
+
+        public Mammal(string animalName, string animalType, double animalWeight, int foodEaten, string livingRegion)
+            : base(animalName, animalType, animalWeight, foodEaten)
+        {
+            _livingRegion = livingRegion;
+        }
+
+        public string LivingRegion
+        {
+            get { return _livingRegion; }
+            set => _livingRegion = value;
+        }
+    }
+}
