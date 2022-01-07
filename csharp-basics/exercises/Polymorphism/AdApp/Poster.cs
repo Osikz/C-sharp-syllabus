6eface68ab636d6c4c7ece92152abb07c554c52e
Osikz <o.jaunslavietis@gmail.com>
2022-01-07 11:41:48 +0200

    Exercise_5 completed

---
+++ csharp-basics/exercises/Polymorphism/AdApp/Poster.cs
@@ -0,0 +1,28 @@
+﻿namespace AdApp
+{
+    public class Poster : Advert
+    {
+        private int _rate;
+        private int _copies;
+        private int _col;
+        private int _row;
+
+        public Poster(int fee, int rate, int copies, int col, int row) : base(fee)
+        {
+            _rate = rate;
+            _copies = copies;
+            _col = col;
+            _row = row;
+        }
+
+        public override int Cost()
+        {
+            return base.Cost() + _col + _row + _rate * _copies;
+        }
+
+        public override string ToString()
+        {
+            return "\nPoster cost: " + Cost();
+        }
+    }
+}
--- csharp-basics/exercises/Polymorphism/AdApp/TVAd.cs
+++ csharp-basics/exercises/Polymorphism/AdApp/TVAd.cs
@@ -2,18 +2,29 @@ namespace AdApp
 {
     public class TVAd: Advert
     {
-        public TVAd(int fee) : base(fee)
+        private bool _peakTime;
+        private int _duration;
+        private int _rate;
+
+        public TVAd(int fee, int duration, int rate, bool peakTime) : base(fee)
         {
+            _duration = duration;
+            _rate = rate;
+            _peakTime = peakTime;
         }
         
-        public new int Cost() 
+        public override int Cost() 
         {
+            if (_peakTime)
+            {
+                return (base.Cost() + _duration * _rate) * 2;
+            }
             return base.Cost();
         }
 
         public override string ToString() 
         {
-            return base.ToString();
+            return "\nTelevision Ad cost: " + Cost();
         }
     }
 }
