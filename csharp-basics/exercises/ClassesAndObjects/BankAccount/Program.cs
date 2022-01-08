488a1fcabf4a6a8223310847b2c68fa30a7a76d7
Osikz <o.jaunslavietis@gmail.com>
2022-01-08 14:14:21 +0200

    BankAccount fixed

--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace BankAccount
 {
@@ -16,28 +15,6 @@ namespace BankAccount
             Console.WriteLine(nebben.ShowUserNameAndBalance());
 
             Console.ReadKey();
-        }
-    }
-
-    class BankAccount
-    {
-        private string _name;
-        private decimal _balance;
-
-        public BankAccount(string name, decimal balance)
-        {
-            _name = name;
-            _balance = balance;
-        }
-
-        public string ShowUserNameAndBalance()
-        {
-            if (_balance < 0)
-            {
-                _balance *= -1;
-                return $"{_name}, -{_balance.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))}";
-            }
-            return $"{_name},  {_balance.ToString("C", CultureInfo.CreateSpecificCulture("en-US"))}";
         }
     }
 }
