d174e0059488c969d2995406170b78feaaba1a15
Osikz <o.jaunslavietis@gmail.com>
2022-01-10 16:54:03 +0200

    Arithmetics_Exercise8 tested

---
+++ csharp-basics/exercises/Arithmetic/Exercise_8/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    public class Methods
+    {
+        public static string CalculatePay(double payRate, int hours)
+        {
+            if (payRate < 8.00 || hours > 60)
+            {
+                return "Something is not right here...";
+            }
+            else if (hours <= 40)
+            {
+                return $"Employee earned {Math.Round(payRate * hours, 2)}$ this week.";
+            }
+            else if (hours > 40)
+            {
+                int overtime = hours - 40;
+                double overtimePayRate = payRate * 1.5;
+                return $"Employee earned {Math.Round(payRate * 40 + overtime * overtimePayRate, 2)}$ this week.";
+            }
+            else
+            {
+                return "Something went wrong..";
+            }
+        }
+    }
+}
