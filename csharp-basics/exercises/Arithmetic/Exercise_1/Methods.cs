a646ce61a5b15ad0d5a72e693cb505fa841da80a
Osikz <o.jaunslavietis@gmail.com>
2022-01-10 16:02:14 +0200

    Arithmetics_Exercise1 tested

---
+++ csharp-basics/exercises/Arithmetic/Exercise_1/Methods.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_1
+{
+    public class Methods
+    {
+        public static bool CheckNumber(int numOne, int numTwo)
+        {
+            if (numOne == 15 || numTwo == 15)
+            {
+                return true;
+            }
+            else if (numOne + numTwo == 15 || numOne - numTwo == 15 || numTwo - numOne == 15)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
