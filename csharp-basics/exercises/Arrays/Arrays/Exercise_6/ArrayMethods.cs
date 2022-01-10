b49c82d112a0cf1338494cb958c9a44b70e04efb
Osikz <o.jaunslavietis@gmail.com>
2022-01-10 15:47:34 +0200

    Arrays_Exercise6 completed

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise_6/ArrayMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    public class ArrayMethods
+    {
+        public static void RandomArrayNumbers(Random rnd, int[] arrayOne, int[] arrayTwo)
+        {
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                arrayOne[i] = rnd.Next(1, 101);
+                arrayTwo[i] = arrayOne[i];
+            }
+        }
+
+        public static void ChangeLastNumber(int[] arrayTwo)
+        {
+            arrayTwo[^1] = -7;
+        }
+    }
+}
